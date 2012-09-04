1afc69bf622e86fa5f42e7094b5f28867ca043ee
stedile <leostedile@gmail.com>
2012-09-04 16:21:37 -0400

    style changes to bridge.cpp

--- src/mongo/tools/bridge.cpp
+++ src/mongo/tools/bridge.cpp
@@ -1,5 +1,4 @@
 // bridge.cpp
-
 /**
  *    Copyright (C) 2008 10gen Inc.
  *
@@ -18,163 +17,172 @@
 
 #include "pch.h"
 
+#include <string>
+
 #include <boost/thread.hpp>
 
 #include "mongo/db/dbmessage.h"
 #include "mongo/util/net/listen.h"
 #include "mongo/util/net/message.h"
 #include "mongo/util/stacktrace.h"
+#include "mongo/util/stringutils.h"
 
 using namespace mongo;
 using namespace std;
 
 int port = 0;
 int delay = 0;
+bool subst = false;
 string destUri;
-void cleanup( int sig );
+void cleanup(int sig);
+std::map<std::string, string> alias; //stores map of host alias
 
 class Forwarder {
 public:
-  Forwarder( MessagingPort &mp ) : mp_( mp ) {
-  }
-  void operator()() const {
-    DBClientConnection dest;
-    string errmsg;
-    while( !dest.connect( destUri, errmsg ) )
-      sleepmillis( 500 );
-    Message m;
-    while( 1 ) {
-      try {
-	m.reset();
-	if ( !mp_.recv( m ) ) {
-	  cout << "end connection " << mp_.psock->remoteString() << endl;
-	  mp_.shutdown();
-	  break;
-	}
-	sleepmillis( delay );
-
-	int oldId = m.header()->id;
-	if ( m.operation() == dbQuery || m.operation() == dbMsg || m.operation() == dbGetMore ) {
-	  bool exhaust = false;
-	  if ( m.operation() == dbQuery ) {
-	    DbMessage d( m );
-	    QueryMessage q( d );
-	    exhaust = q.queryOptions & QueryOption_Exhaust;
-	  }
-	  Message response;
-	  dest.port().call( m, response );
-
-	  // nothing to reply with?
-	  if ( response.empty() ) cleanup(0);
-
-	  mp_.reply( m, response, oldId );
-	  while ( exhaust ) {
-	    MsgData *header = response.header();
-	    QueryResult *qr = (QueryResult *) header;
-	    if ( qr->cursorId ) {
-	      response.reset();
-	      dest.port().recv( response );
-	      mp_.reply( m, response ); // m argument is ignored anyway
-	    }
-	    else {
-	      exhaust = false;
-	    }
-	  }
-	}
-	else {
-	  dest.port().say( m, oldId );
-	}
-      }
-      catch ( ... ) {
-	log() << "caught exception in Forwarder, continuing" << endl;
-      }
+    Forwarder(MessagingPort &mp) : mp_(mp) {
     }
-  }
+    
+    void operator()() const {
+        DBClientConnection dest;
+        string errmsg;
+        while(!dest.connect(destUri, errmsg))
+            sleepmillis(500);
+        Message m;
+        while(1) {
+            try {
+                m.reset();
+                if (!mp_.recv(m)) {
+                    cout << "end connection " << mp_.psock->remoteString() << endl;
+                    mp_.shutdown();
+                    break;
+                }
+                sleepmillis(delay);
+                
+                int oldId = m.header()->id;
+                if (m.operation() == dbQuery || m.operation() == dbMsg || m.operation() == dbGetMore) {
+                    bool exhaust = false;
+                    if (m.operation() == dbQuery) {
+                        DbMessage d(m);
+                        QueryMessage q(d);
+                        exhaust = q.queryOptions & QueryOption_Exhaust;
+                    }
+                    Message response;
+                    dest.port().call(m, response);
+                    
+                    // nothing to reply with?
+                    if (response.empty())
+                        cleanup(0);
+                    mp_.reply(m, response, oldId);
+                    while (exhaust) {
+                        MsgData *header = response.header();
+                        QueryResult *qr = (QueryResult *) header;
+                        if (qr->cursorId) {
+                            response.reset();
+                            dest.port().recv(response);
+                            mp_.reply(m, response); // m argument is ignored anyway
+                        }
+                        else {
+                            exhaust = false;
+                        }
+                    }
+                }
+                else {
+                    dest.port().say(m, oldId);
+                }
+            }
+            catch (...) {
+                log() << "caught exception in Forwarder, continuing" << endl;
+            }
+        }
+    }
+    
 private:
-  MessagingPort &mp_;
+    MessagingPort &mp_;
 };
 
-set<MessagingPort*>& ports ( *(new std::set<MessagingPort*>()) );
+set<MessagingPort*>& ports (*(new std::set<MessagingPort*>()));
 
 class MyListener : public Listener {
 public:
-  MyListener( int port ) : Listener( "bridge" , "", port ) {}
-  virtual void acceptedMP(MessagingPort *mp) {
-    ports.insert( mp );
-    Forwarder f( *mp );
-    boost::thread t( f );
-  }
+    MyListener(int port) : Listener("bridge" , "", port) {}
+    virtual void acceptedMP(MessagingPort *mp) {
+        ports.insert(mp);
+        Forwarder f(*mp);
+        boost::thread t(f);
+    }
 };
 
 auto_ptr< MyListener > listener;
 
 
-void cleanup( int sig ) {
-  ListeningSockets::get()->closeAll();
-  for ( set<MessagingPort*>::iterator i = ports.begin(); i != ports.end(); i++ )
-    (*i)->shutdown();
-  ::_exit( 0 );
+void cleanup(int sig) {
+    ListeningSockets::get()->closeAll();
+    for (set<MessagingPort*>::iterator i = ports.begin(); i != ports.end(); i++)
+        (*i)->shutdown();
+    ::_exit(0);
 }
 #if !defined(_WIN32)
 void myterminate() {
-  rawOut( "bridge terminate() called, printing stack:" );
-  printStackTrace();
-  ::abort();
+    rawOut("bridge terminate() called, printing stack:");
+    printStackTrace();
+    ::abort();
 }
 
 void setupSignals() {
-  signal( SIGINT , cleanup );
-  signal( SIGTERM , cleanup );
-  signal( SIGPIPE , cleanup );
-  signal( SIGABRT , cleanup );
-  signal( SIGSEGV , cleanup );
-  signal( SIGBUS , cleanup );
-  signal( SIGFPE , cleanup );
-  set_terminate( myterminate );
+    signal(SIGINT , cleanup);
+    signal(SIGTERM , cleanup);
+    signal(SIGPIPE , cleanup);
+    signal(SIGABRT , cleanup);
+    signal(SIGSEGV , cleanup);
+    signal(SIGBUS , cleanup);
+    signal(SIGFPE , cleanup);
+    set_terminate(myterminate);
 }
 #else
 inline void setupSignals() {}
 #endif
 
 void helpExit() {
-  cout << "usage mongobridge --port <port> --dest <destUri> [ --delay <ms> ]" << endl;
-  cout << "    port: port to listen for mongo messages" << endl;
-  cout << "    destUri: uri of remote mongod instance" << endl;
-  cout << "    ms: transfer delay in milliseconds (default = 0)" << endl;
-  ::_exit( -1 );
+    cout << "usage mongobridge --port <port> --dest <destUri> [ --delay <ms> ]" << endl;
+    cout << "    port: port to listen for mongo messages" << endl;
+    cout << "    destUri: uri of remote mongod instance" << endl;
+    cout << "    ms: transfer delay in milliseconds (default = 0)" << endl;
+    ::_exit( -1 );
 }
 
-void check( bool b ) {
-  if ( !b )
-    helpExit();
+void check(bool b) {
+    if (!b)
+        helpExit();
 }
 
-int main( int argc, char **argv ) {
-  static StaticObserver staticObserver;
-
-  setupSignals();
-
-  check( argc == 5 || argc == 7 );
-
-  for( int i = 1; i < argc; ++i ) {
-    check( i % 2 != 0 );
-    if ( strcmp( argv[ i ], "--port" ) == 0 ) {
-      port = strtol( argv[ ++i ], 0, 10 );
+int main(int argc, char **argv) {
+    static StaticObserver staticObserver;
+    
+    setupSignals();
+    
+    check( argc == 5 || argc == 7 );
+    
+    for(int i = 1; i < argc; ++i) {
+        check(i % 2 != 0);
+        if (strcmp(argv[i], "--port") == 0) {
+            port = strtol(argv[++i], 0, 10);
+        }
+        else if (strcmp(argv[i], "--dest") == 0) {
+            destUri = argv[++i];
+        }
+        else if (strcmp(argv[i], "--delay") == 0) {
+            delay = strtol(argv[++i], 0, 10);
+        }
+        else {
+            check( false );
+        }
     }
-    else if ( strcmp( argv[ i ], "--dest" ) == 0 ) {
-      destUri = argv[ ++i ];
-    }
-    else if ( strcmp( argv[ i ], "--delay" ) == 0 ) {
-      delay = strtol( argv[ ++i ], 0, 10 );
-    }
-    else {
-      check( false );
-    }
-  }
-  check( port != 0 && !destUri.empty() );
-
-  listener.reset( new MyListener( port ) );
-  listener->initAndListen();
-
-  return 0;
+    
+    check(port != 0 && !destUri.empty());
+    
+    
+    listener.reset(new MyListener(port));
+    listener->initAndListen();
+    
+    return 0;
 }
