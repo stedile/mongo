e4416348a44e43e7364796f6ddb22eedb97cadc4
stedile <leostedile@gmail.com>
2012-09-04 16:27:39 -0400

    added subst option for bridge.cpp and two js tests

---
+++ jstests/tool/mongobridge_portchange2.js
@@ -0,0 +1,53 @@
+r = new ReplSetTest({name : "addshard4", nodes : 3, startPort : 31100});
+r.startSet();
+var config = r.getReplSetConfig();
+config.members[1].priority = 0;
+config.members[2].arbiterOnly = true;
+r.initiate(config);
+//Wait for replica set to be fully initialized - could take some time
+//to pre-allocate files on slow systems
+r.awaitReplication();
+db = connect("localhost:31100/test");
+host = rs.status().members[0].name.split(':')[0];
+str = host + ":31100=localhost:31105,";
+str += host + ":31101=localhost:31106,";
+str += host + ":31102=localhost:31107";
+//start a bridge to secondary, test ReplSetGetStatus(memebers.name)
+// and isMaster (primary, me, hosts, passive, arbiter)
+startMongoProgram("mongobridge", "--port", "29001", "--dest", "localhost:31101", 
+		  "--subst", str);
+
+db = connect("localhost:29001/test");
+count = 0;
+count2 = 0;
+count3 = 0;
+for (i = 0; i < 3; i++){
+    if (rs.status().members[i].name == "localhost:31105")
+        count++;
+    if (rs.status().members[i].name == "localhost:31106")
+	count2++;
+    if (rs.status().members[i].name == "localhost:31107")
+	count3++;
+}
+
+assert.eq(count, 1);
+assert.eq(count2, 1);
+assert.eq(count3, 1);
+
+assert.eq (db.isMaster().primary, "localhost:31105");
+count = 0;
+count2 = 0;
+count3 = 0;
+
+for (i = 0; i < 1; i++){
+    if (db.isMaster().hosts[i] == "localhost:31105")
+        count++;
+    if (db.isMaster().passives[i] == "localhost:31106")
+	count2++;
+    if (db.isMaster().arbiters[i] == "localhost:31107")
+	count3++;
+}
+
+assert.eq(count, 1);
+assert.eq(count2, 1);
+assert.eq(count3, 1);
--- src/mongo/tools/bridge.cpp
+++ src/mongo/tools/bridge.cpp
@@ -67,19 +67,35 @@ public:
                         exhaust = q.queryOptions & QueryOption_Exhaust;
                     }
                     Message response;
+                    Message newResponse;
                     dest.port().call(m, response);
-                    
-                    // nothing to reply with?
                     if (response.empty())
                         cleanup(0);
-                    mp_.reply(m, response, oldId);
+                    mongo::QueryResult* qrpResponse = (mongo::QueryResult*)response.singleData();
+                    if (m.operation() == dbQuery) {
+                        if (subst) {
+                            DbMessage d(m);
+                            QueryMessage qmResponse(d);
+                            string s = qmResponse.query.firstElement().fieldName();
+                            if (s == "isMaster")
+                                replaceIsMaster(qrpResponse, &newResponse);
+                            
+                            else if (s == "replSetGetStatus")
+                                replaceReplSetGetStatus(qrpResponse, &newResponse);
+                        }
+                        
+                        else
+                            newResponse = response;
+                    }
+                    
+                    mp_.reply(m, newResponse, oldId);
                     while (exhaust) {
-                        MsgData *header = response.header();
+                        MsgData *header = newResponse.header();
                         QueryResult *qr = (QueryResult *) header;
                         if (qr->cursorId) {
-                            response.reset();
-                            dest.port().recv(response);
-                            mp_.reply(m, response); // m argument is ignored anyway
+                            newResponse.reset();
+                            dest.port().recv(newResponse);
+                            mp_.reply(m, newResponse); // m argument is ignored anyway
                         }
                         else {
                             exhaust = false;
@@ -96,9 +112,151 @@ public:
         }
     }
     
+    void replaceIsMaster(QueryResult* qr, Message* newResponse) const {
+        mongo::BSONObj data(qr->data());
+        BSONObjBuilder newQuery;
+        // Copy all elements of the original query object, changing some fields
+        BSONObjIterator fieldIterator(data);
+        while (fieldIterator.more()) {
+            BSONElement field = fieldIterator.next();
+            
+            if (mongoutils::str::equals(field.fieldName(), "hosts")) {
+                BSONArrayBuilder newHost (newQuery.subarrayStart("hosts"));
+                BSONObjIterator hostNameIterator (field.embeddedObject());
+                while (hostNameIterator.more()) {
+                    BSONElement hostName = hostNameIterator.next();
+                    string host = hostName.str();
+                    if (alias.find(host) != alias.end()) {
+                        host = alias[host];
+                    }
+                    newHost.append(host);
+                }
+                newHost.done();
+            }
+            
+            else if (mongoutils::str::equals(field.fieldName(), "arbiters")) {
+                BSONArrayBuilder newArbiter (newQuery.subarrayStart("arbiters"));
+                BSONObjIterator arbiterIterator (field.embeddedObject());
+                while (arbiterIterator.more()) {
+                    BSONElement arbiter = arbiterIterator.next();
+                    string arb = arbiter.str();
+                    if (alias.find(arb) != alias.end())
+                        arb = alias[arb];
+                    newArbiter.append(arb);
+                }
+                newArbiter.done();
+            }
+            
+            else if (mongoutils::str::equals(field.fieldName(), "passives")) {
+                BSONArrayBuilder newPassive (newQuery.subarrayStart("passives"));
+                BSONObjIterator passiveIterator (field.embeddedObject());
+                while (passiveIterator.more()) {
+                    BSONElement passive = passiveIterator.next();
+                    string psv = passive.str();
+                    if (alias.find(psv) != alias.end())
+                        psv = alias[psv];
+                    newPassive.append(psv);
+                }
+                newPassive.done();
+            }
+            
+            else if (mongoutils::str::equals(field.fieldName(), "me")) {
+                string me = field.str();
+                if (alias.find(me) != alias.end())
+                    me = alias[me];
+                newQuery.append(field.fieldName(), me);
+            }
+            
+            else if (mongoutils::str::equals(field.fieldName(), "primary")) {
+                string primary = field.str();
+                if (alias.find(primary) != alias.end())
+                    primary = alias[primary];
+                newQuery.append(field.fieldName(), primary);
+            }
+            
+            else
+                newQuery.append(field);
+        }
+        BSONObj newData = newQuery.obj();
+        //now we need to copy back to the QueryResult
+        BufBuilder b(32768);
+        b.skip(sizeof(QueryResult));
+        {
+            b.appendBuf(newData.objdata() , newData.objsize());
+        }
+        QueryResult *newQr = (QueryResult*)b.buf();
+        newQr->_resultFlags() = qr->_resultFlags();
+        newQr->len = b.len();
+        newQr->setOperation(opReply);
+        newQr->cursorId = qr->cursorId;
+        newQr->startingFrom = qr->startingFrom;
+        newQr->nReturned = qr->nReturned;
+        b.decouple();
+        newResponse->setData(newQr , true);
+    }
+    
+    void replaceReplSetGetStatus(QueryResult* qr, Message* newResponse) const {
+        mongo::BSONObj data(qr->data());
+        BSONObjBuilder newQuery;
+        BSONObjIterator fieldIterator(data);
+        while (fieldIterator.more()) {
+            BSONElement field = fieldIterator.next();
+            
+            if (mongoutils::str::equals(field.fieldName(), "members")) {
+                
+                BSONArrayBuilder newMemberArray (newQuery.subarrayStart("members"));
+                BSONObjIterator memberIterator (field.embeddedObject());
+                
+                while (memberIterator.more()) {
+                    BSONElement member = memberIterator.next();
+                    
+                    BSONObjBuilder newMemberField;
+                    BSONObjIterator memberFieldIterator = BSONObj (member.embeddedObject());
+                    
+                    while (memberFieldIterator.more()) {
+                        BSONElement memberField = memberFieldIterator.next();
+                        if (str::equals(memberField.fieldName(), "name")) {
+                            string s = memberField.str();
+                            if (alias.find(s) != alias.end())
+                                s = alias[s];
+                            newMemberField.append(memberField.fieldName(), s);
+                        }
+                        else
+                            newMemberField.append(memberField);
+                    }
+                    
+                    newMemberArray.append(newMemberField.obj());
+                    
+                }
+                newMemberArray.done();
+            }
+            else
+                newQuery.append(field);
+        }
+        
+        BSONObj newData = newQuery.obj();
+        //now we need to copy back to the QueryResult
+        BufBuilder b(32768);
+        b.skip(sizeof(QueryResult));
+        {
+            b.appendBuf(newData.objdata() , newData.objsize());
+        }
+        QueryResult *newQr = (QueryResult*)b.buf();
+        newQr->_resultFlags() = qr->_resultFlags();
+        newQr->len = b.len();
+        newQr->setOperation(opReply);
+        newQr->cursorId = qr->cursorId;
+        newQr->startingFrom = qr->startingFrom;
+        newQr->nReturned = qr->nReturned;
+        b.decouple();
+        newResponse->setData(newQr , true);
+    }
+    
 private:
     MessagingPort &mp_;
 };
+
+
 
 set<MessagingPort*>& ports (*(new std::set<MessagingPort*>()));
 
@@ -143,11 +301,18 @@ inline void setupSignals() {}
 #endif
 
 void helpExit() {
-    cout << "usage mongobridge --port <port> --dest <destUri> [ --delay <ms> ]" << endl;
+    cout << "usage mongobridge --port <port> --dest <destUri> [--delay <ms> --subst <list>]"
+    << endl;
     cout << "    port: port to listen for mongo messages" << endl;
     cout << "    destUri: uri of remote mongod instance" << endl;
     cout << "    ms: transfer delay in milliseconds (default = 0)" << endl;
-    ::_exit( -1 );
+    cout << "    list: list of uri substitutions; hosts in a pair are separated by = and";
+    cout << " pairs are comma separated. Substitutes occurences of the first host in a pair";
+    cout << " with the second one in replies to replSetGetStatus and isMaster through the bridge.";
+    cout << endl;
+    cout << "          (e.g. host1=host2,host3=host4 subtitutes";
+    cout << " host1 with host2, and host3 with host 4)" << endl;
+    ::_exit(-1);
 }
 
 void check(bool b) {
@@ -157,11 +322,9 @@ void check(bool b) {
 
 int main(int argc, char **argv) {
     static StaticObserver staticObserver;
-    
     setupSignals();
-    
-    check( argc == 5 || argc == 7 );
-    
+    check(argc == 5 || argc == 7 || argc == 9);
+    string aliasList;
     for(int i = 1; i < argc; ++i) {
         check(i % 2 != 0);
         if (strcmp(argv[i], "--port") == 0) {
@@ -173,8 +336,29 @@ int main(int argc, char **argv) {
         else if (strcmp(argv[i], "--delay") == 0) {
             delay = strtol(argv[++i], 0, 10);
         }
+        else if (strcmp(argv[i], "--subst") == 0) {
+            subst = true;
+            aliasList = argv[++i];
+        }
         else {
-            check( false );
+            check(false);
+        }
+    }
+    //Parse the substitution argument
+    std::vector<std::string> list;
+    
+    if (subst) {
+        splitStringDelim(aliasList, &list, ',');
+        for (size_t i = 0; i < list.size(); i++) {
+            std::vector<std::string> list2;
+            splitStringDelim(list[i], &list2, '=');
+            if (list2.size() != 2) {
+                cerr << list[i];
+                cerr << " is not a valid format for subst argument.";
+                cerr << "Use --help for more info on correct format\n";
+                return -1;
+            }
+            alias[list2[0]] = list2[1];
         }
     }
     
